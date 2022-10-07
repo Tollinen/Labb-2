941bf12d96d6f6ef70547d7fea35325ac98c2573
Tollinen <adam.tollin@hotmail.com>
2022-10-07 14:18:04 +0200

    V1

---
+++ Appliances.cs
@@ -0,0 +1,85 @@
+﻿namespace Labb_2
+{
+    internal class Appliances
+    {
+        public interface IKitchenAppliance
+        {
+            string Type { get; set; }
+            string Brand { get; set; }
+            public bool IsFunctioning { get; set; }
+            void Use();
+            Appliance createAppliance();
+        }
+
+        public abstract class baseAppliance : IKitchenAppliance
+        {
+            public string Type { get; set; }
+            public string Brand { get; set; }
+            public bool IsFunctioning { get; set; }
+            public abstract void Use();
+            public abstract Appliance createAppliance();
+        }
+
+
+        public class Appliance : baseAppliance
+        {
+            public Appliance(string type, string brand, bool functioning)
+            {
+                this.Type = type;
+                this.Brand = brand;
+                this.IsFunctioning = functioning;
+            }
+
+            public override void Use() //Kollar om apparaten är trasig eller inte och skriver ut ett meddelande beroende på resultatet.
+            {
+                if (IsFunctioning)
+                {
+                    Console.WriteLine($"Använder {Type}({Brand}).");
+                }
+                else
+                {
+                    Console.WriteLine($"{Type}({Brand}) är trasig och kan inte användas.");
+                }
+            }
+
+            public override Appliance createAppliance() //Låter användaren skapa en ny köksapparat
+            {
+                string märke;
+                string typ;
+                bool fungerar = true;
+                bool loop = true;
+
+                Console.WriteLine("Märke:");
+                märke = Console.ReadLine();
+                Console.WriteLine("Typ:");
+                typ = Console.ReadLine();
+                Console.WriteLine("Fungerar den?(j = Ja, n = Nej)");
+
+                while (loop)                            // Sätter isFunctionng till true eller false om man skriver in j eller n.
+                {
+                    string input = Console.ReadLine();
+
+                    if (input.ToLower() == "j")
+                    {
+                        fungerar = true;
+                        loop = false;
+                    }
+                    else if (input.ToLower() == "n")
+                    {
+                        fungerar = false;
+                        loop = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bara j och n godtas.");
+                    }
+                }
+
+                Appliance nyAppliance = new Appliance(märke, typ, fungerar);
+                return nyAppliance;
+            }
+
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,130 @@
+﻿using static Labb_2.Appliances;
+
+bool menyLoop = true;
+List<IKitchenAppliance> applianceList = new List<IKitchenAppliance>() { new Appliance("Ugn", "Electrolux", true), new Appliance("Mikrovågsugn", "Fimbaldvi", false) };
+
+while (menyLoop)
+{
+    Console.Clear();
+    Console.WriteLine(
+        "[1] Använd köksapparat\n" +
+        "[2] Lägg till köksapparat\n" +
+        "[3] Lista köksapparater\n" +
+        "[4] Ta bort köksapparat\n" +
+        "[5] Avsluta");
+
+
+    switch (MenyVal(5))
+    {
+        case 1:         //Använd köksapparat
+            Console.Clear();
+            if (applianceList.Count <= 0)       // Körs om Köksapparatlistan är tom
+            {
+                Console.WriteLine("Det finns inga köksapparater. Lägg till några.\n" +
+                                  "[Enter] för att gå tillbaka till menyn.");
+                Console.ReadLine();
+            }
+            else   //Körs om det finns någonting i Köksapparatlistan
+            {
+                Console.WriteLine("##### Använd köksapparat #####");
+                for (int i = 0; i < applianceList.Count; i++)            //Skriver ut alla köksapparater
+                {
+                    Console.WriteLine($"[{(i + 1)}] {applianceList[i].Type}, {applianceList[i].Brand}");
+                }
+                applianceList[(MenyVal((applianceList.Count())) - 1)].Use();
+                Console.WriteLine("[Enter] för att gå tillbaka till menyn.");
+                Console.ReadLine();
+            }
+            break;
+
+        case 2:         // Lägg till köksapparat
+            Console.Clear();
+            Console.WriteLine("##### Lägg till köksapparat #####");
+            applianceList.Add(new Appliance("", "", true).createAppliance()); // Skapar en ny köksapparat som läggs till i listan
+            Console.WriteLine("Apparaten har lagts till!");
+            Console.WriteLine("[Enter] för att gå tillbaka till menyn.");
+            Console.ReadLine();
+
+            break;
+
+        case 3:         // Lista köksapparater
+            Console.Clear();
+            Console.WriteLine("-------------------");
+            foreach (var item in applianceList)
+            {
+                Console.WriteLine($"Typ: {item.Type}\n" +
+                    $"Märke: {item.Brand}\n" +
+                    $"Skick: {item.IsFunctioning}");
+                Console.WriteLine("-------------------");
+            }
+            Console.WriteLine("[Enter] för att gå tillbaka till menyn.");
+            Console.ReadLine();
+            break;
+
+        case 4:         // Ta bort köksapparat
+            Console.Clear();
+            if (applianceList.Count <= 0)       // Körs om Köksapparatlistan är tom
+            {
+                Console.WriteLine("Det finns inga köksapparater. Lägg till några.\n" +
+                                  "[Enter] för att gå tillbaka till menyn.");
+                Console.ReadLine();
+            }
+            else   //Körs om det finns någonting i Köksapparatlistan
+            {
+                Console.WriteLine("##### Ta bort köksapparat #####");
+                for (int i = 0; i < applianceList.Count; i++)            //Skriver ut alla köksapparater
+                {
+                    Console.WriteLine($"[{(i + 1)}] {applianceList[i].Type}, {applianceList[i].Brand}");
+                }
+                applianceList.RemoveAt((MenyVal((applianceList.Count())) - 1)); // Tar bort köksapparaten
+                Console.WriteLine("Köksapparaten har tagits bort.");
+                Console.WriteLine("[Enter] för att gå tillbaka till menyn.");
+                Console.ReadLine();
+            }
+            break;
+
+        case 5:         // Avsluta
+            menyLoop = false;
+            break;
+
+        default:
+            break;
+    }
+
+}
+
+
+
+
+
+//##### Metoder #####
+int MenyVal(int valMängd) //Används för menyval. valmängd används för att skriva ut max värdet som godtas.
+{
+    bool valLoop = false;
+    var menyval = 0;
+    do
+    {
+        try
+        {
+            
+            menyval = Convert.ToInt32(Console.ReadLine());
+            if (menyval < 1 || menyval > valMängd)
+            {
+                Console.WriteLine($"Bara nummer mellan 1-{valMängd} godtas");
+                valLoop = true;
+            }
+            else
+            {
+                valLoop = false;
+            }
+        }
+        catch
+        {
+            Console.WriteLine($"Bara nummer mellan 1-{valMängd} godtas");
+            valLoop = true;
+        }
+    }
+    while (valLoop);
+
+    return menyval;
+}
